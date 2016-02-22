9d4e5582db643d863ebe03847b2894c87b41ada6
EmanuelVig <ic15b066@technikum-wien.at>
2016-02-22 23:08:14 +0100

    Airplane class and Move interface

---
+++ tasks/Task2/Task2/Airplane.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Airplane: IMove
+    {
+        public Airplane(int length, string name)
+        {
+            Length = length;
+            Name = name;
+        }
+        public int Length { get; set; }
+        public string Name { get; set; }
+        private bool inFlyMode = false;
+
+        public bool GetFlyStatus()
+        {
+            return this.inFlyMode;
+        }
+
+        public void TakeOf()
+        {
+            inFlyMode = true;
+        }
+
+        public void land()
+        {
+            inFlyMode = false;
+        }
+    }
+    
+    public interface IMove
+    {
+        void TakeOf();
+        void land();
+    }  
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Airplane airplane = new Airplane(40, "phönix");
+            airplane.TakeOf();
+            Console.WriteLine("airplane {0} {1} have take of {2}", airplane.Name, airplane.Length, airplane.GetFlyStatus());
+            airplane.land();
+            Console.WriteLine("airplane {0} {1} have take of {2}", airplane.Name, airplane.Length, airplane.GetFlyStatus());
+            Console.ReadLine();
+        }
+    }
+}
